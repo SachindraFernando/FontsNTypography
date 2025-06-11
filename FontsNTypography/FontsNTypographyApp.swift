42ff82789652f73b05446d47fca7f547e646fa58
Sachindra Fernando <sachindrafernando3@gmail.com>
2025-06-11 13:17:32 +0100

    Initial Commit

---
+++ FontsNTypography/FontsNTypographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontsNTypographyApp.swift
+//  FontsNTypography
+//
+//  Created by Sachindra Fernando on 2025-06-11.
+//
+
+import SwiftUI
+
+@main
+struct FontsNTypographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
