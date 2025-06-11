fa60a807279272cbdbdb0c095df0291817ba68c2
Sachindra Fernando <sachindrafernando3@gmail.com>
2025-06-11 19:36:26 +0100

    Added more font styles

--- FontsNTypography/ContentView.swift
+++ FontsNTypography/ContentView.swift
@@ -99,6 +99,118 @@ struct ContentView: View {
                     .font(.system(.body, design: .rounded))
                     .fontWeight(.heavy)
             }
+            
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Hello, Ultra Light Font")
+                    .fontWeight(.ultraLight)
+                Text("Hello, Thin Font")
+                    .fontWeight(.thin)
+                Text("Hello, Light Font")
+                    .fontWeight(.light)
+                Text("Hello, Regular Font")
+                    .fontWeight(.regular)
+                Text("Hello, Medium Font")
+                    .fontWeight(.medium)
+                Text("Hello, Semibold Font")
+                    .fontWeight(.semibold)
+                Text("Hello, Bold Font")
+                    .fontWeight(.bold)
+                Text("Hello, Heavy Font")
+                    .fontWeight(.heavy)
+                Text("Hello, Black Font")
+                    .fontWeight(.black)
+            }
+            
+            Divider()
+            
+            Text("Hello, Italicized Text")
+               .italic()
+            Text("Hello, Underlined Text")
+               .underline()
+            Text("Hello, Strikethrough Text")
+               .strikethrough()
+            
+            Divider()
+            
+            Text("Hello, Pattern dot")
+                .strikethrough(pattern: .dot)
+            Text("Hello, Color pink")
+               .strikethrough(pattern: .dash, color: .pink)
+            Text("Hello, Pattern dot")
+               .underline(pattern: .dot, color: .accentColor)
+            
+            Divider()
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("default")
+                    Text("monospacedDigit").monospacedDigit()
+                }
+
+                VStack(alignment: .leading) {
+                    Text("1234567890")
+                    Text("1234567890").monospacedDigit()
+                }
+            }
+            
+            Divider()
+            
+            Text("Kerning Text - 2")
+            .kerning(2)
+            Text("Tracking Text - 2")
+            .tracking(2)
+            
+            HStack {
+                Text("some text")
+                Text("baselineOffset").baselineOffset(15)
+                Text("set")
+                Text("to").baselineOffset(15).underline()
+                Text("15").underline()
+            }
+            
+            Divider()
+            
+            HStack(alignment: .firstTextBaseline) {
+                Text("some text")
+                Text("baselineOffset").baselineOffset(15)
+                Text("set")
+                Text("to").baselineOffset(15).underline()
+                Text("15").underline()
+            }
+            
+            Divider()
+            
+            HStack(alignment: .firstTextBaseline) {
+                Text("set text width")
+
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("expanded")
+                        .fontWidth(.expanded)
+                    Text("standard")
+                        .fontWidth(.standard)
+                    Text("condensed")
+                        .fontWidth(.condensed)
+                    Text("compressed")
+                        .fontWidth(.compressed)
+                }
+            }
+            
+            Divider()
+            
+            Text("American Typewriter")
+                .font(.custom("AmericanTypewriter", size: 18))
+            Text("Copperplate")
+                .font(.custom("Copperplate", size: 18))
+            Text("Cochin font - fixedSize 18")
+                .font(.custom("Cochin", fixedSize: 18)) // available iOS 14 and macOS 11
+            Text("Cochin font - size 18")
+                .font(.custom("Cochin", size: 18))
+            Text("Cochin font - fixedSize 18 - relativeTo body")
+                .font(.custom("Cochin", size: 18, relativeTo: .body))
+            
+            Divider()
         }
     }
 }
